5a633d7b911f7b26971fdbcf8097858b74641403
M.ubaidullah <ubaid.naeem315@gmail.com>
2026-03-26 16:54:20 +0500

    done

--- app/src/main/java/com/example/smd_assigment_1/MainActivity.java
+++ app/src/main/java/com/example/smd_assigment_1/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
                 replaceFragment(new CartFragment());
                 return true;
             } else if (id == R.id.nav_profile) {
-                replaceFragment(PlaceholderFragment.newInstance("Profile"));
+                replaceFragment(new AccountFragment());
                 return true;
             }
             return false;
